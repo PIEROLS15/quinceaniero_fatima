a7a5fa6c9227cbb762cc78e712747e828960e024
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2025-02-23 15:18:57 -0500

    fix(Data): Se mejoró el Metada y la Data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,27 +1,29 @@
 const Data = {
-    "primer_nombre": "Alcira",
-    "segundo_nombre": "Flor",
-    "fecha": "2025-03-01T21:00:00",
+    "primer_nombre": "Fátima",
+    "segundo_nombre": "Rosmery",
+    "tercer_nombre": "Nicole",
+    "descripcion": "Ven a mis XV años",
+    "fecha": "2025-03-08T19:00:00",
     "padres": {
-        "madre": "Nemesia Damián Choccllo",
-        "padre": "Fidencio Torres Meza"
+        "madre": "Carmen Rosa Cisneros Ramos",
+        "padre": "José Nicolás Espino Calderón"
     },
     "padrinos": {
-        "padrino": "Juan Torres Carranza",
-        "madrina": "Evelyn Bernuy Rojas"
+        "padrino": "David Joan Tataje Mendoza",
+        "madrina": "Jenny Marina Hernández"
     },
     "saludo": "Con la bendición de Dios y el amor que mis padres me han dado, me siento feliz de llegar a este momento de mi vida, el día que dejaré mi infancia y comenzaré un nuevo viaje con muchas metas por cumplir",
     "invitacion": "Mi mayor deseo es compartir con las personas que han estado para mí. Tú eres una de ellas.",
-    "primer_agradecimiento": "Hoy, hace quince años, mis padres agradecrían a Dios por mi llegada al mundo. Hoy, soy yo quien agradece a Dios por tenerlos a ellos, por su amor incondicional, su paciencia y sus sabios consejos.",
-    "segundo_agradecimiento": "Agradezco también a mi familia y amigos por ser parte de mi vida y por hacer este día aún más especial con su cariño y compañía.",
-    "dedicatoria":"Querida hija, que los hermosos recuerdos de tu niñez queden grabados en tu corazón como un tesoro invaluable. En esta nueva etapa que comienza hoy, te deseo un futuro lleno de amor, alegría y bendiciones. Que cada paso que de esté guiado por la felicidad y el éxito, y que sigas brillando con las misma luz que nos ilumina a todos.",
+    "primer_agradecimiento": 'Gracias a Dios por darme los padres que amo y por darme la vida, con el orgullo de ser su hija, hoy dejo de ser su niña consentida para convertirme en su princesa amada. Como en un cuento de hadas y estrellas mañaneras hoy mi sueño se hace realidad "Mi fiesta de 15 años"',
+    "segundo_agradecimiento": "Existen momentos que soñamos e idealizamos, hoy estoy radiante de alegría, porque uno de esos momentos ha llegado, acompañenme en esta mágica noche que sera inolvidable",
+    "dedicatoria": "Querida hija, que los hermosos recuerdos de tu niñez queden grabados en tu corazón como un tesoro invaluable. En esta nueva etapa que comienza hoy, te deseo un futuro lleno de amor, alegría y bendiciones. Que cada paso que de esté guiado por la felicidad y el éxito, y que sigas brillando con las misma luz que nos ilumina a todos.",
     "dia": "Sábado",
-    "numero": "01",
+    "numero": "08",
     "mes": "Marzo",
     "anio": "2025",
-    "hora": "21:00",
-    "lugar": "Salón de Recepción Eventos Krystal",
-    "direccion": "San Marcos de la Aguada - 28 de julio Sur Al Fondo Mz D Lote 6",
+    "hora": "19:00",
+    "lugar": "Local El achiote de Mi Abuelo",
+    "direccion": "Mala - Referencia: El pino - Grifo Primax",
     "ubicacion": "https://goo.gl/maps/1J1J1J1J1J1J1J1J1",
     "asistencia": "La pista de baile está lista, ¡Solo falta tu confirmación! Por favor confirma tu asistencia hasta el 28 de febrero del 2025",
     "confirmar": "https://forms.gle/1J1J1J1J1J1J1J1J"
